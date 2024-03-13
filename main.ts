e524db3e842067ccc30fe0d0b4305b688835a499
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-03-13 21:07:25 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,76 @@
+// //Comparison Oprator
+// //Equal  == 
+let num1: number = 5;
+let num2: number = 5;
+console.log(num1 == num2);//True
+
+let num3: number = 6;
+let num4: number = 10;
+console.log(num3 == num4);//False
+
+let name1: string = "SAHIL";
+let name2: string = "SAHIL";
+console.log(name1 == name2);//True
+
+let name3: string = "SAHIL";
+let name4: string = "CHRIS";
+console.log(name3 == name4);//False
+
+
+// //Not equal !=  
+let num5: number = 10;
+let num6: number = 20;
+console.log(num5 != num6);//True
+
+let num7: number = 10;
+let num8: number = 10;
+console.log(num7 != num8);//Fasle
+
+let name5: string = "unithan";
+let name6: string = "almithia";
+console.log(name5 != name6);//True
+
+let names7: string = "raza";
+let names8: string = "raza";
+console.log(names7 != names8);//True
+
+// //Grater Then >
+let age1: number = 30;
+let age2: number = 20
+console.log(age1 > age2);//True
+
+let oldAge: number = 50;
+let newAge: number = 65;
+console.log(oldAge > newAge);//Fasle
+
+
+// //Greater then or Equal >= 
+let age4: number = 20;
+let age5: number = 20;
+console.log(age4 >= age5);//True
+
+let age6:number=23;
+let age7:number=25;
+console.log(age6>=age7);//Fasle
+
+
+// //Less Then <
+let salary1: number = 5000;
+let salary2: number = 10000;
+console.log(salary1 < salary2);//True
+
+let salary4: number = 10000;
+let salary5: number = 5000;
+console.log(salary4 < salary5);//Fasle
+
+//Less Then Equal <=
+let  salary9: number = 5000;
+let salary10: number =  5000;
+console.log(salary9<=salary10)//True
+
+let salary11: number = 10000;
+let salary12: number =  9000;
+console.log(salary11 <= salary12);//Fasle
+
+
+
